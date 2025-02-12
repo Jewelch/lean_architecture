ca65464467286680b08c765494dcf65efea11903
jewelcheriaa <jewelcheriaa@gmail.com>
2025-02-12 18:47:32 +0100

    Refactor use case base classes for generic type handling

--- lib/modules/demo/product/domain/usecases/get_product_by_id.dart
+++ lib/modules/demo/product/domain/usecases/get_product_by_id.dart
@@ -4,7 +4,7 @@ import '../../../common/entities/product.dart';
 import '../../../common/models/product_model.dart';
 import '../../data/datasource/product_datasource.dart';
 
-class GetProductByIdUC extends SingleResultUseCase<Product, ProductModel, Params> {
+class GetProductByIdUC extends UseCase<Product, ProductModel, Product> {
   GetProductByIdUC(ProductDataSource dataSource)
       : super(
           dataSourceFetcher: (params) => dataSource.getProductById(params.id),
